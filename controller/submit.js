a81c1fc6f642dc55d8066eb6e633c6dec39c5494
Naman Kumar <naman@outlook.in>
2020-11-09 00:46:58 +0530

    Fix gcc

--- controller/submit.js
+++ controller/submit.js
@@ -1,10 +1,10 @@
-const { exec } = require("child_process");
-const uuid = require("uuid").v4;
-const fs = require("fs");
+const { exec } = require('child_process');
+const uuid = require('uuid').v4;
+const fs = require('fs');
 
 const runPython = (code, input) =>
   new Promise((resolve) => {
-    const filename = uuid() + ".py";
+    const filename = uuid() + '.py';
 
     fs.writeFileSync(filename, code);
     exec(
@@ -13,23 +13,23 @@ const runPython = (code, input) =>
             .split(/(\n| )/)
             .map((i) => i.trim() && `echo ${i}`)
             .filter((i) => !!i)
-            .join(" & ")}) | python ${filename}`
+            .join(' & ')}) | python ${filename}`
         : `python ${filename}`,
       (error, stdout, stderr) => {
         exec(`rm ${filename}`);
         resolve(
           stdout.toString() ||
-            (error && error.message.split("<module>")[1].trim()) ||
-            stderr.toString()
+            (error && error.message.split('<module>')[1].trim()) ||
+            stderr.toString(),
         );
-      }
+      },
     );
   });
 
 const runCpp = (code, input) =>
   new Promise((resolve) => {
     const outputFilename = uuid();
-    const filename = outputFilename + ".cpp";
+    const filename = outputFilename + '.cpp';
 
     fs.writeFileSync(filename, code);
     exec(
@@ -38,21 +38,27 @@ const runCpp = (code, input) =>
             .split(/(\n| )/)
             .map((i) => i.trim() && `echo ${i}`)
             .filter((i) => !!i)
-            .join(
-              " & "
-            )}) | gcc ${filename} -o ${outputFilename} && ${outputFilename}`
-        : `gcc ${filename}`,
+            .join(' & ')}) | gcc ${filename} -o ${outputFilename} && ./${outputFilename}`
+        : `gcc ${filename} -o ${outputFilename} && ./${outputFilename}`,
       (error, stdout, stderr) => {
+        console.log(error, stdout, stderr);
         exec(`rm ${filename} ${outputFilename}`);
-        resolve(
-          stdout.toString() || (error && error.message) || stderr.toString()
-        );
-      }
+        resolve(stdout.toString() || (error && error.message) || stderr.toString());
+      },
     );
   });
 
-exports.postSubmission = async (req, res, next) => {
-  const { code, input = "0" } = req.body || {};
+const langCompilerMap = {
+  python: runPython,
+  cpp: runCpp,
+};
 
-  res.send({ output: await runCpp(code, input) });
+exports.postSubmission = async (req, res, next) => {
+  const { code, lang, input = '0' } = req.body || {};
+
+  const compiler = langCompilerMap[lang];
+
+  if (!compiler) return res.status(402).send({ error: 'The lang is not supported.' });
+
+  res.send({ output: await compiler(code, input) });
 };
